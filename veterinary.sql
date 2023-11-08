a63018d11e37d9112c49324c90f13a61a84a68dd
Brian Neil Babasa <brianneil238@gmail.com>
2023-11-08 12:41:25 +0800

    Owners table

--- veterinary.sql
+++ veterinary.sql
@@ -1,2 +1,10 @@
 CREATE DATABASE veterinary_database;
 
+-- feat/create-table-owners
+CREATE TABLE owners(
+	ownerid INT PRIMARY KEY,
+	ofirstname VARCHAR(50) NOT NULL,
+	olastname VARCHAR(50) NOT NULL,
+	address VARCHAR(100) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	phone VARCHAR(50) NOT NULL
